43d60d05b82d69e630bdd2ad3a76b32d8f2fd62a
jimmybnguyen <jimmyn9@uw.edu>
2016-10-22 13:57:45 -0700

    change report to load all movies

--- challenge3/js/app.js
+++ challenge3/js/app.js
@@ -1,18 +1,130 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function() {
+    // Making sure JS loads
     console.log("Document Readay");
+    
+    // Checking global variable
+    console.log(MOVIES);
 });
 
-var dropdown = document.querySelector("#dropdown");
-var table = document.querySelector(".table");
+var dropdown = document.querySelector("#report-select");
+var table = document.querySelector("#report");
 
-var starWars = MOVIES.filter(function (movie) {
-    return movie.title.toLowerCase() === "star wars";
+var starWars = MOVIES.filter(function (item) {
+    return item.title.toLowerCase() === "star wars";
 });
+
+
+function buildTable() {
+    // table body and table head
+    var tbody = document.createElement("tbody");
+    var thead = document.createElement("thead");
+    
+    // Row for the header
+    var threadRow = document.createElement("tr");
+    
+    // Columns for the header
+    var titleTh = document.createElement("th");
+    titleTh.textContent = "Title";
+
+    var releasedTh = document.createElement("th");
+    releasedTh.textContent = "Released";
+
+    var distributorTh = document.createElement("th");
+    distributorTh.textContent = "Distributor";
+    
+    var genreTh = document.createElement("th");
+    genreTh.textContent = "Genre";
+
+    var ratingTh = document.createElement("th");
+    ratingTh.textContent = "Rating";
+
+    var yearTh = document.createElement("th");
+    yearTh.textContent = "Year";
+    
+    var salesTh = document.createElement("th");
+    salesTh.textContent = "Sales";
+
+    var ticketsTh = document.createElement("th");
+    ticketsTh.textContent = "Tickets";
+    
+    // Append these elements to the table
+    threadRow.appendChild(titleTh);
+    threadRow.appendChild(releasedTh);
+    threadRow.appendChild(distributorTh);
+    threadRow.appendChild(genreTh);
+    threadRow.appendChild(ratingTh);
+    threadRow.appendChild(yearTh);
+    threadRow.appendChild(salesTh);
+    threadRow.appendChild(ticketsTh);
+    
+    thead.appendChild(threadRow);
+    table.appendChild(tbody);
+    table.appendChild(thead);
+}
 
 function buildRows(rows) {
+    // First, build the table structure.
+    buildTable();
+
+    // Find the table body, where the rows will be rendered.
+    var tbody = document.querySelector("tbody");
     
+    // Iterate over each movie title,
+    // create the tr (row element) and td elements (column elements)
+    // and append to the table body.
+    rows.forEach(function (name) {
+        var nameTr = document.createElement("tr");
+
+        // Object.keys returns an array of the keys object
+        var nameKeys = Object.keys(name);
+
+        // This makes it easy to iterate over the values
+        // in the object by using bracket notation
+        // to access each property in the object.
+        nameKeys.forEach(function (key) {
+            var value = name[key];
+
+            var td = document.createElement("td");
+            td.textContent = value;
+
+            nameTr.appendChild(td);
+        });
+
+        tbody.appendChild(nameTr);
+    });
+    
+    // Calculate the total for the given array of titles.
+    // Array.reduce takes an array of items,
+    // and returns a single value based on logic you provided.
+    // In this case, we want to sum all the name counts
+    // for the given list of movies.
+    var total = rows.reduce(function (sum, name) {
+        var newSum = sum + name.count;
+        return newSum;
+    }, 0); // 0 is the initial value for the sum
+    
+    // Build totals rows
+    var tableFooter = document.createElement("tfoot");
+    var totalsTr = document.createElement("tr");
+    var totalsNameTd = document.createElement("td");
+    totalsNameTd.textContent = "Total";
+
+    // We don't want anything in the second column
+    var blankTd = document.createElement("td");
+
+    var totalsTotalTd = document.createElement("td");
+    totalsTotalTd.textContent = total;
+
+    // Append totals row
+    totalsTr.appendChild(totalsNameTd);
+    totalsTr.appendChild(blankTd);
+    totalsTr.appendChild(totalsTotalTd);
+
+    // Append footer (with totals)
+    tableFooter.appendChild(totalsTr);
+    table.appendChild(tableFooter);
 }
 
 // When the selection in the dropdown changes,
@@ -28,8 +140,8 @@ dropdown.addEventListener("change", function (e) {
 
     if (value === "star-wars") {
         buildRows(starWars);
-    } else if (value === "20th") {
-        buildRows(th);
+    } else if (value === "remake20") {
+        buildRows(remake20);
     } else if (value === "avg-by-genre") {
         buildRows(avgByGenre);
     } else if (value === "top-by-tickets") {
@@ -41,3 +153,4 @@ dropdown.addEventListener("change", function (e) {
 
 // On page load, show the data for all movies
 buildRows(MOVIES);
+
