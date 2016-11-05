444f281fb446893fb6a5feac8f005c5099ccc52d
jimmybnguyen <jimmyn9@uw.edu>
2016-11-05 11:44:05 -0700

    firebase init

--- challenge4/public/js/app.js
+++ challenge4/public/js/app.js
@@ -1,3 +1,9 @@
-/*
- * This file should call firebase.initializeApp({ ... }) with your app credentials.
- */
+"use strict";
+var config = {
+    apiKey: "AIzaSyAUsTCW4wVtRpeuW6vRmNSUH01Dt8FoE2M",
+    authDomain: "fireslack-cafa1.firebaseapp.com",
+    databaseURL: "https://fireslack-cafa1.firebaseio.com",
+    storageBucket: "fireslack-cafa1.appspot.com",
+    messagingSenderId: "974723622528"
+};
+firebase.initializeApp(config);
